998fc2d89345ec3b3f622593fd6e0de177434536
Sandro <ssillva@bol.com.br>
2017-07-02 02:30:34 -0300

    Update twitter_plot.py
    você não está fechando o arquivo aberto, então existe essa forma de leitura que o Python fecha automaticamente o arquivo para voce.

--- scripts/twitter_plot.py
+++ scripts/twitter_plot.py
@@ -9,13 +9,13 @@ tweets_data_path = 'twitter_data.txt'
 
 if __name__ == '__main__':
     tweets_data = []
-    tweets_file = open(tweets_data_path, "r")
-    for line in tweets_file:
-        try:
-            tweet = json.loads(line)
-            tweets_data.append(tweet)
-        except:
-            continue
+    with open(tweets_data_path, 'r') as tweets_file:
+        for line in tweets_file:
+            try:
+                tweet = json.loads(line)
+                tweets_data.append(tweet)
+            except:
+                continue
 
 
     print len(tweets_data)
